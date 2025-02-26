e799f791c6d7c91005fa5337df4c4b63d760c589
Jaciel Melnik <jacielmelnik@gmail.com>
2025-02-26 09:03:13 -0300

    Add result module

---
+++ lib/result/result_screen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class ResultScreen extends StatefulWidget {
+  const ResultScreen({super.key});
+
+  @override
+  State<ResultScreen> createState() => _ResultScreenState();
+}
+
+class _ResultScreenState extends State<ResultScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Kalshi')),
+      body: Center(child: Text('result screen')),
+    );
+  }
+}
