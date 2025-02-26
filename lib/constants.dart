5b29a975892c4ab00ba0bd3dcc4183e39cd96274
Jaciel Melnik <jacielmelnik@gmail.com>
2025-02-26 13:17:57 -0300

    Update constant files

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,3 +1,9 @@
 const kalshiLogo = 'assets/kalshi_logo_black.png';
+const financialWellnessIcon = 'assets/financial_wellness_icon.png';
+const dolarSignIcon = 'assets/dollar_sign_icon.png';
+const lockIcon = 'assets/lock_icon.png';
 
 final onlyNumbersMask = RegExp('([0-9])');
+
+const String footerText =
+    'Your financial information is encrypted and secure. We\'ll never share or sell any of your personal data.';
---
+++ lib/enter_values/enter_values_constants.dart
@@ -0,0 +1,7 @@
+const String titlePart1 = 'Let\'s find out your ';
+const String titlePart2 = 'financial wellness score.';
+
+const String cardTitle = 'Financial wellness test';
+const String cardSubtitle = 'Enter your financial information below';
+
+const String continueButtonText = 'Continue';
